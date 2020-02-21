fcb500d3cc2f95077ffc66bde26be7849c7c4f76
段昊辰 <duanhaochen@126.com>
2020-02-21 11:49:36 +0800

    ci: 添加https环境配置

--- pm2.config.js
+++ pm2.config.js
@@ -3,11 +3,16 @@ module.exports = {
     {
       name: 'pocket48-web',
       script: 'dist/server/index.js',
+      error_file: './logs/app-err.log',
+      out_file: './logs/app-out.log',
       env: {
         NODE_ENV: 'production'
       },
-      error_file: './logs/app-err.log',
-      out_file: './logs/app-out.log'
+      env_https: {
+        NODE_ENV: 'production',
+        KEY: 'server.key',
+        CERT: 'server.crt'
+      }
     }
   ]
 };
